46c377de5bf1e89aa0a2a3b20be184d36ee4500d
Jozsef Komjati <jozsef.komjati@dunelm.com>
2021-07-02 01:18:33 +0100

    Add typescript support to webpack via babel-preset-typescript

---
+++ src/client/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { render } from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "bootstrap/dist/css/bootstrap-reboot.min.css";
+
+render(<App />, document.getElementById("root"));
